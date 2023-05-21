f77f8c0066230d658f52344cedead4587949f4af
lhqbao <57691739+lhqbao@users.noreply.github.com>
2023-05-21 21:42:25 +0700

    add

---
+++ connect.php
@@ -0,0 +1,6 @@
+<?php
+	//$Connect = mysqli_connect("localhost","root","","qlbh") or die("Lỗi".mysqli_error($Connect));
+	$Connect = mysqli_connect("mysql://so5hw6hozabe8nvx:t1kwhbx7sn2e8i5a@en1ehf30yom7txe7.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/dqy88q9ae4ngx369");
+	mysqli_query($Connect,'SET NAMES "utf8"');
+	//mysqli_close($Connect);
+?>
