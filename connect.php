827587325de3542dc3d5f395eeb56568fc32a130
lhqbao <57691739+lhqbao@users.noreply.github.com>
2023-06-07 11:28:00 +0700

    update connect.php

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
-	$Connect = mysqli_connect("exbodcemtop76rnz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","e7wns9ynkfqr2hq5","duenoewt7va3pslz","n45blpleivvybh5f") or die("Lỗi".mysqli_error($Connect));
+	$Connect = mysqli_connect("co28d739i4m2sb7j.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","zlhrbs7jobq4nye5","dbkx381drl05vv5v","le8ccyl3nh36n7w5") or die("Lỗi".mysqli_error($Connect));
 	//$Connect = mysqli_connect("mysql://so5hw6hozabe8nvx:t1kwhbx7sn2e8i5a@en1ehf30yom7txe7.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/dqy88q9ae4ngx369");
 	mysqli_query($Connect,'SET NAMES "utf8"');
 	//mysqli_close($Connect);
